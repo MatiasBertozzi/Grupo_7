6a95779fc04c1af36e93db972b10d6c774f8f376
MatiasBertozzi <bertozzim@udesa.edu.ar>
2024-11-11 10:24:54 -0300

    modificacion de la base de datos

--- myApp/controllers/userControler.js
+++ myApp/controllers/userControler.js
@@ -5,6 +5,20 @@ const users={
         res.send('respond with a resource');
       },
       registro:function(req, res, next) {
+
+        let form = req.body;/* capturo datos del formulario, tienen que coincidir los datos del formulario con las columnas de a base de datos */
+        form.password = bcryptjs.hashSync(form.password, 10);
+      
+        db.User.create(form)  /* modelo de usuario para guardar la info del form en la base de datos */
+        .then((results) =>{
+            return res.redirect("/");
+        })
+        .catch((err) => {
+            return console.log(err);
+            ;
+        });       
+        
+        
         res.render('register.ejs');
       },
       login:function(req, res, next) {
--- myApp/database/baseDeDatos.sql
+++ myApp/database/baseDeDatos.sql
@@ -2,8 +2,8 @@ create table users (
 id int unsigned primary key auto_increment,
 usuario varchar(50) not null,
 email text not null	,
-contraseña text not null, 
-created_at 	timestamp default current_timestamp	not null,
+password_user text not null, 
+created_at 	timestamp default current_timestamp ,
 updated_at	timestamp default current_timestamp on update current_timestamp	
 );
 
@@ -12,7 +12,7 @@ create table product(
 id int unsigned primary key auto_increment,
 id_user int unsigned not null  ,
 nombre_producto varchar(50) not null,
-created_at 	timestamp default current_timestamp	not null,
+created_at 	timestamp default current_timestamp	,
 updated_at	timestamp default current_timestamp on update current_timestamp	,
 descripcion text not null ,
 foreign key (id_user) references users (id)
