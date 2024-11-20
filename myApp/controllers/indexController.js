4f1e035a58c582fde34dbfa3de085c4105f80673
VilaMariano <vilam@udesa.edu.ar>
2024-11-20 16:56:13 -0300

    Modifique el detalle del controlador y estoy trabjando con el index

--- myApp/controllers/indexController.js
+++ myApp/controllers/indexController.js
@@ -1,5 +1,20 @@
+const db = require('../database/models')
+
 module.exports = {
-    index: function (req,res) {
-            return res.render('index')
+    index: function (req, res) {
+        let filtro = {
+            include: [{ association: "user" }],
+            order: [["createdAt", "DESC"]], 
+        };
+        db.Product.findAll(filtro)
+            .then(function (productos) {
+                res.render("index", { productos });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    },
     }
-}
+    
+
+
--- myApp/controllers/productController.js
+++ myApp/controllers/productController.js
@@ -1,4 +1,19 @@
-const product={
+const db = require('../database/models')
+
+const product ={
+    detalle: function (req,res){
+      let id = req.params.id;
+      let filtro = {
+          include: [{association: "user"}]
+      }
+      db.Product.findByPk (id, filtro)
+      .them(function(results) {
+        return res.render('product',{producto: results})
+      })
+      .catch(function(error){
+        console.log(error)
+      })
+    },
     searchProduct:function(req, res, next) {
          res.render("search-results.ejs");
       },
