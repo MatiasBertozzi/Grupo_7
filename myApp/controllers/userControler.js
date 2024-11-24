a30a29ed0acae9165f3000ec71ea10b0a8a5b8a1
Beltran132 <gcaceras@gmail.com>
2024-11-24 13:10:32 -0300

    logg

--- myApp/controllers/userControler.js
+++ myApp/controllers/userControler.js
@@ -64,6 +64,33 @@ const users={
         res.clearCookie("id_user");
         return res.redirect("/")
       },
+
+control:function(req ,res) {
+  function isLogg(req, res, next) {
+    if (req.session.user) {
+        return res.redirect('/'); // Si está logueado, redirige al home
+    }
+    next();
+}
+
+function noLogg(req, res, next) {
+    if (!req.session.user) {
+        return res.redirect('/login'); // Si no está logueado, redirige al login
+    }
+    next();
+}
+
+  
+}
+
+
+
+
+
+
+
+
+
      
     
       
